2c481d3d38d221f7172e6116aa7f1c208de1fb48
Julius von Froreich <fvj@users.noreply.github.com>
2018-01-03 03:10:45 +0100

    Add cache-control and fix stroke colour

--- color.go
+++ color.go
@@ -14,9 +14,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		color = "#" + color
 	}
 	w.Header().Set("Content-Type", "image/svg+xml")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("ETag", "clrsv0.0.1")
 	w.Write([]byte(`
 		<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-			<rect stroke="#000" stroke-width="2" fill="` + color + `" x="1" y="1" width="14" height="14" rx="0" ry="0" />
+			<rect stroke="#111" stroke-width="1" fill="` + color + `" x="1" y="1" width="14" height="14" rx="0" ry="0" />
 		</svg>`))
 }
 
